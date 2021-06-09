a279c0b14c0c25f4cb51cb7ec441aa4b67f08165
Tomoya Tanjo <ttanjo@gmail.com>
2021-06-09 07:20:58 +0000

    WIP

--- combiner.py
+++ combiner.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python
 import hail as hl
 
-import os
-import sys
-
+from itertools import islice
+from argparse import ArgumentParser
 from pyspark import SparkContext, SparkConf
 
 if __name__ == '__main__':
-        conf = {
-                'spark.executor.memory': '42g',
-                'spark.driver.memory': '50g'
-        }
+    parser = ArgumentParser()
+    parser.add_argument('input-file', metavar='path_to_input_list',
+                        help='a file that lists VCFs (i.e., `*.vcf` or `*.vcf.gz`)')
+    parser.add_argument('-n', type=str,
+                        help='only use first n items from the input file (default: unlimited)')
+    parser.add_argument('--batch-size', metavar='batch_size', type=str, default='100',
+                        help='batch size of the backend Spark cluster')
+    parser.add_argument('--log', metavar='logfile', default='output.log',
+                        help='log output file name')
+    parser.add_argument('-o', '--output', default='output.mt',
+                        help='output directory name')
+    parser.add_argument('--executor-memory', metavar='executor_memory', default='42g',
+                        help='memory allocation for spark executor')
+    parser.add_argument('--driver-memory', metavar='driver_memory', default='50g',
+                        help='memory allocation for spark driver')
+    parser.add_argument('--executor-cores', metavar='executor_cores', type=str,
+                        help='#cores for each executor')
+    args = parser.parse_args()
 
-        paths = []
-        workdir = os.getcwd()
-        hl.init(spark_conf=conf, log=workdir+'/combiner.log')
+    conf = {
+        'spark.executor.memory': args.executor_memory,
+        'spark.driver.memory': args.driver_memory,
+    }
+    if args.executor_cores is not None:
+        conf['spark.executor.cores'] = args.executor_cores
 
-        args = sys.argv
-
-        inputs = []
-        if len(args) == 1:
-                inputs = paths
+    inputs = []
+    with hl.hadoop_open(args.path_to_input_list, 'r') as f:
+    # with open(args.path_to_input_list, 'r') as f:
+        if args.n is None:
+            inputs = [line.strip() for line in f]
         else:
-                path_to_input_list = args[1]
+            inputs = list(islice((line.strip() for line in f), 0, args.n))
 
-#               with hl.hadoop_open(path_to_input_list, 'r') as f:
-                with open(path_to_input_list, 'r') as f:
-                    for line in f:
-                        inputs.append(line.strip())
+    temp_path = 'temp'
 
-        output_file = 'output.mt'
-        temp_path = 'temp'
-        hl.experimental.run_combiner(inputs, out_file=output_file, tmp_path=temp_path,
-                                     use_genome_default_intervals=True)
+    hl.init(spark_conf=conf, log=args.logfile)
+    hl.experimental.run_combiner(inputs, out_file=args.output, tmp_path=temp_path,
+                                 use_genome_default_intervals=True)
